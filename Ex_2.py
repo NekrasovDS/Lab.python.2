f68cf3532203c4a2b2bde6852a8b08f0b2d0a41f
NekrasovDS <112845788+NekrasovDS@users.noreply.github.com>
2024-04-05 12:10:54 +0300

    Update Ex_2.py

--- Ex_2.py
+++ Ex_2.py
@@ -1,1 +1,22 @@
+# Задание 2. Вариант 8.
+
+# apple - malum, pomum, popula
+# fruit - baca, bacca, popum
+# punishment - malum, multa
+
+n = int(input())
+dictionary = {}
+
+for _ in range(n):
+    line = input().split(" - ")
+    english_word = line[0]
+    latin_translations = sorted(line[1].split(", "))
+    for translation in latin_translations:
+        if translation not in dictionary:
+            dictionary[translation] = []
+        dictionary[translation].append(english_word)
+
+print(len(dictionary))
+for latin_word in sorted(dictionary.keys()):
+    print(f"{latin_word} - {', '.join(sorted(dictionary[latin_word]))}")
 
